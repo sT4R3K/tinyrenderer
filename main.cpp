7fd7509372fa38b74401f7873d25236baf982a89
Tarek Mokhtari <mokhtar15u@fst-ats-315-l11.fst.site.univ-lorraine.fr>
2017-03-15 09:42:21 +0100

    fst attempt

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include "tgaimage.h"
 
+void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
 int main(int argc, char** argv) {
         TGAImage image(100, 100, TGAImage::RGB);
-        image.set(52, 41, red);
-        image.flip_vertically(); 
 
+	line (12, 12, 24, 24, image, white);
+
+        image.flip_vertically(); 
         image.write_tga_file("output.tga");
+	
         return 0;
+}
+
+void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+	for (float t = 0.; t < 1.; t += .01) {
+		int x = x0*(1.-t) + x1*t;
+        	int y = y0*(1.-t) + y1*t;
+		image.set (x, y, color);
+	}
 }
