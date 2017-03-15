6df39ea3785f13da9d726153dad7a440b67eba9f
Tarek Mokhtari <mokhtar15u@fst-ats-315-l11.fst.site.univ-lorraine.fr>
2017-03-15 09:50:01 +0100

    3rd attempt

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "tgaimage.h"
+#include <cstdlib>
 
 void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 
@@ -20,10 +21,24 @@ int main(int argc, char** argv) {
         return 0;
 }
 
-void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+    bool steep = false; 
+    if (std::abs(x0-x1)<std::abs(y0-y1)) { // if the line is steep, we transpose the image 
+        std::swap(x0, y0); 
+        std::swap(x1, y1); 
+        steep = true; 
+    } 
+    if (x0>x1) { // make it left−to−right 
+        std::swap(x0, x1); 
+        std::swap(y0, y1); 
+    } 
     for (int x=x0; x<=x1; x++) { 
         float t = (x-x0)/(float)(x1-x0); 
         int y = y0*(1.-t) + y1*t; 
-        image.set(x, y, color); 
+        if (steep) { 
+            image.set(y, x, color); // if transposed, de−transpose 
+        } else { 
+            image.set(x, y, color); 
+        } 
     } 
 }
