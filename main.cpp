3dc9e648374292f30d6be76508c269bafd4ef5af
Tarek <tarek.mokhtari@hotmail.it>
2017-03-16 16:14:34 +0100

    refaire le 3e essaie

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+using namespace std;
+
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
 
@@ -20,15 +22,32 @@ int main(int argc, char** argv) {
 }
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-	const int Dx = x1 - x0;
-	const int Dy = y1 - y0;
+	int Dx = abs (x1 - x0);
+	int Dy = abs (y1 - y0);
+
+	bool steep = false;
+	if (Dx < Dy) {
+		swap (x0, y0);
+		swap (x1, y1);
+		swap (Dx, Dy);
+		steep = true;
+	}
+
+	if (x0 > x1) {
+		swap (x0, x1);
+		swap (y0, y1);
+	}
 
 	int y;
 	float t;
 
-	for (int x = x0; x < x1; x++) {
+	for (int x = x0; x <= x1; x++) {
 		t = (x - x0)/(float) Dx;
 		y = y0 + Dy*t;
-		image.set(x, y, color);
+
+		if (steep)
+			image.set (y, x, color);
+		else
+			image.set (x, y, color);
 	}
 } 
