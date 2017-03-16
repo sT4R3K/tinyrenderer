0179d054cc02ac7ba62730f18df6d4a798e0014b
Tarek <tarek.mokhtari@hotmail.it>
2017-03-16 14:43:46 +0100

    refaire le 1er essaie

--- main.cpp
+++ main.cpp
@@ -7,46 +7,28 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
 int main(int argc, char** argv) {
-        TGAImage image(100, 100, TGAImage::RGB);
-
-	//line (12, 10, 56, 63, image, white);
+	TGAImage image(100, 100, TGAImage::RGB);
 
 	line(13, 20, 80, 40, image, white); 
 	line(20, 13, 40, 80, image, red); 
 	line(80, 40, 13, 20, image, red);
-        
+
 	image.flip_vertically(); 
-        image.write_tga_file("output.tga");
+	image.write_tga_file("output.tga");
 	
-        return 0;
+	return 0;
 }
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-    bool steep = false; 
-    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
-        std::swap(x0, y0); 
-        std::swap(x1, y1); 
-        steep = true; 
-    } 
-    if (x0>x1) { 
-        std::swap(x0, x1); 
-        std::swap(y0, y1); 
-    } 
-    int dx = x1-x0; 
-    int dy = y1-y0; 
-    float derror = std::abs(dy/float(dx)); 
-    float error = 0; 
-    int y = y0; 
-    for (int x=x0; x<=x1; x++) { 
-        if (steep) { 
-            image.set(y, x, color); 
-        } else { 
-            image.set(x, y, color); 
-        } 
-        error += derror; 
-        if (error>.5) { 
-            y += (y1>y0?1:-1); 
-            error -= 1.; 
-        } 
-    } 
+	const int dx = x1 - x0;
+	const int dy = y1 - y0;
+
+	int x;
+	int y;
+
+	for (float t = .0; t < 1.; t += 0.1) {
+		x = x0 + dx*t;
+		y = y0 + dy*t;
+		image.set(x,y,color);
+	}
 } 
