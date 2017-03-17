b1b19f162645b39818c655f7254fdc5ec558c83e
Tarek <tarek.mokhtari@hotmail.it>
2017-03-17 09:57:32 +0100

    1er triangle

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 #include "model.h"
 #include "geometry.h"
 
-void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void line (int, int, int, int, TGAImage&, TGAColor);
+void triangle (Vec2i, Vec2i, Vec2i, TGAImage&, TGAColor);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int width  = 800;
-const int height = 800;
+const TGAColor green = TGAColor(0,   255, 0,   255);
+
+const int width  = 260;
+const int height = 260;
 
 using namespace std;
 
@@ -24,6 +27,8 @@ int main(int argc, char** argv) {
     }
 
 	TGAImage image(width, height, TGAImage::RGB);
+
+    /*
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         for (int j=0; j<3; j++) {
@@ -36,20 +41,27 @@ int main(int argc, char** argv) {
             line(x0, y0, x1, y1, image, white);
         }
     }
-
+	//*/
     /*
 		line (400,720,20,20,image,red);
 		line (20,20,600,60,image,red);
 	    line (600,60,400,720,image,red);
 	//*/
-	
+
+	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)}; 
+	Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)}; 
+	Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)}; 
+	triangle(t0[0], t0[1], t0[2], image, red); 
+	triangle(t1[0], t1[1], t1[2], image, white); 
+	triangle(t2[0], t2[1], t2[2], image, green);
+
 	image.flip_vertically(); 
 	image.write_tga_file("output.tga");
 	
 	return 0;
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
 	int Dx = abs (x1 - x0);
 	int Dy = abs (y1 - y0);
 
@@ -85,3 +97,11 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 		}
 	}
 } 
+
+void triangle (Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+	line (t0.x, t0.y, t1.x, t1.y, image, color);
+	line (t1.x, t1.y, t2.x, t2.y, image, color);
+	line (t2.x, t2.y, t0.x, t0.y, image, color);
+
+	
+}
