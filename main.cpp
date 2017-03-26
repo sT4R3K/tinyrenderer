32b702b506fbc4b7a362f2838ebd02f1ed03f66b
Tarek <tarek.mokhtari@hotmail.it>
2017-03-26 05:01:52 +0200

    [ADD] makefile + [MODIF] line ()

--- main.cpp
+++ main.cpp
@@ -6,37 +6,28 @@
 #include "model.h"
 #include "geometry.h"
 
-void line (int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+void triangle (Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color);
+void line (Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int width  = 800;
-const int height = 800;
+const TGAColor green = TGAColor(0,   255, 0,   255);
+
+const int width  = 200;
+const int height = 200;
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	Model *model;
-
-	if (argc == 2) {
-		model = new Model (argv[1]);
-	} else {
-		model = new Model ("obj/african_head.obj");
-	}
-
 	TGAImage image(width, height, TGAImage::RGB);
-	for (int i=0, j; i<model->nfaces(); i++) {
-		std::vector<int> face = model->face(i);
-		for (j=0; j<3; j++) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x+1.)*width/2.;
-			int y0 = (v0.y+1.)*height/2.;
-			int x1 = (v1.x+1.)*width/2.;
-			int y1 = (v1.y+1.)*height/2.;
-			line(x0, y0, x1, y1, image, white);
-		}
-	}
+
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 
 	image.flip_vertically(); 
 	image.write_tga_file("output.tga");
@@ -44,21 +35,27 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
-	int Dx = x1 - x0;
-	int Dy = y1 - y0;
+void triangle (Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+	line (t0, t1, image, color);
+	line (t1, t2, image, color);
+	line (t2, t0, image, color);
+}
+
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) { 
+	int Dx = p1.x - p0.x;
+	int Dy = p1.y - p0.y;
 
 	bool steep = false;
 	if (abs (Dx) < abs (Dy)) {
-		swap (x0, y0);
-		swap (x1, y1);
+		swap (p0.x, p0.y);
+		swap (p1.x, p1.y);
 		swap (Dx, Dy);
 		steep = true;
 	}
 
-	if (x0 > x1) {
-		swap (x0, x1);
-		swap (y0, y1);
+	if (p0.x > p1.x) {
+		swap (p0.x, p1.x);
+		swap (p0.y, p1.y);
 		Dx *= -1;
 		Dy *= -1;
 	}
@@ -69,7 +66,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 	int step = (Dy>0 ? 1 : -1);
 
-	for (int x = x0, y = y0; x <= x1; x++) {
+	for (int x = p0.x, y = p0.y; x <= p1.x; x++) {
 		if (steep)
 			image.set (y, x, color);
 		else
