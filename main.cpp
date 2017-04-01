3acd54e7bca4701b6eb85ecefe4f346098f11cc3
Tarek <tarek.mokhtari@hotmail.it>
2017-04-01 10:12:47 +0200

    Chap3: begin with a simple 2D scene

--- main.cpp
+++ main.cpp
@@ -9,46 +9,31 @@
 
 Vec3f barycentric (Vec2i *pts, Vec2i P);
 void triangle (Vec2i *pts, TGAImage &image, TGAColor color);
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
+const TGAColor blue = TGAColor(0,   0, 255,   255);
 
-const int width  = 3200;
-const int height = 3200;
+const int width  = 800;
+const int height = 800;
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	Model *model;
+	TGAImage scene(width, height, TGAImage::RGB);
 
-	if (argc == 2) {
-		model = new Model (argv[1]);
-	} else {
-		model = new Model ("obj/african_head.obj");
-	}
+	// scene "2d mesh"
+	line(Vec2i(20, 34),   Vec2i(744, 400), scene, red);
+	line(Vec2i(120, 434), Vec2i(444, 400), scene, green);
+	line(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
 
-	TGAImage image(width, height, TGAImage::RGB);
-	Vec3f light_dir(0,0,-1);
-	for (int i=0; i<model->nfaces(); i++) { 
-		std::vector<int> face = model->face(i); 
-		Vec2i screen_coords[3]; 
-		Vec3f world_coords[3]; 
-		for (int j=0; j<3; j++) { 
-			Vec3f v = model->vert(face[j]); 
-			screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.); 
-			world_coords[j]  = v; 
-		} 
-		Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]); 
-		n.normalize(); 
-		float intensity = n*light_dir; 
-		if (intensity>0) { 
-			triangle(screen_coords, image, TGAColor(intensity*255, intensity*255, intensity*255, 255)); 
-		} 
-	}
+	// screen line
+	line(Vec2i(10, 10), Vec2i(790, 10), scene, white);
 
-	image.flip_vertically(); 
-	image.write_tga_file("output.tga");
+	scene.flip_vertically(); 
+	scene.write_tga_file("output.tga");
 	
 	return 0;
 }
@@ -79,4 +64,43 @@ void triangle (Vec2i  *pts, TGAImage &image, TGAColor color) {
 			if (bc_P.x >= 0 &&  bc_P.y >= 0 && bc_P.z >= 0)
 				image.set (P.x, P.y, color);
 		}
+}
+
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) { 
+	int Dx = p1.x - p0.x;
+	int Dy = p1.y - p0.y;
+
+	bool steep = false;
+	if (abs (Dx) < abs (Dy)) {
+		swap (p0.x, p0.y);
+		swap (p1.x, p1.y);
+		swap (Dx, Dy);
+		steep = true;
+	}
+
+	if (p0.x > p1.x) {
+		swap (p0.x, p1.x);
+		swap (p0.y, p1.y);
+		Dx *= -1;
+		Dy *= -1;
+	}
+
+	int Dy2 = abs (Dy) * 2;
+	int Dx2 = abs (Dx) * 2;
+	int error2 = 0;
+
+	int step = (Dy>0 ? 1 : -1);
+
+	for (int x = p0.x, y = p0.y; x <= p1.x; x++) {
+		if (steep)
+			image.set (y, x, color);
+		else
+			image.set (x, y, color);
+
+		error2 += Dy2;
+		if (error2 > Dx) {
+			y += step;
+			error2 -= Dx2;
+		}
+	}
 }
