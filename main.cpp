1a94a601cb2953f94d6f44472281dae857c6c325
Tarek <tarek.mokhtari@hotmail.it>
2017-04-01 08:20:30 +0200

    [FIN] Chapitre 2

--- main.cpp
+++ main.cpp
@@ -29,14 +29,22 @@ int main(int argc, char** argv) {
 	}
 
 	TGAImage image(width, height, TGAImage::RGB);
+	Vec3f light_dir(0,0,-1);
 	for (int i=0; i<model->nfaces(); i++) { 
-	    std::vector<int> face = model->face(i); 
-	    Vec2i screen_coords[3]; 
-	    for (int j=0; j<3; j++) { 
-	        Vec3f world_coords = model->vert(face[j]); 
-	        screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.); 
-	    } 
-	    triangle(screen_coords, image, TGAColor(rand()%255, rand()%255, rand()%255, 255)); 
+		std::vector<int> face = model->face(i); 
+		Vec2i screen_coords[3]; 
+		Vec3f world_coords[3]; 
+		for (int j=0; j<3; j++) { 
+			Vec3f v = model->vert(face[j]); 
+			screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.); 
+			world_coords[j]  = v; 
+		} 
+		Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]); 
+		n.normalize(); 
+		float intensity = n*light_dir; 
+		if (intensity>0) { 
+			triangle(screen_coords, image, TGAColor(intensity*255, intensity*255, intensity*255, 255)); 
+		} 
 	}
 
 	image.flip_vertically(); 
